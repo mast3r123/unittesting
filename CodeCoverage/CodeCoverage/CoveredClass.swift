9a8433ea7a71f35f29c032f42d64c395173cb7bf
mast3r123 <abhishek.mast3r@gmail.com>
2022-08-04 22:01:03 -0400

    code coverage isn't always a good indicator

--- CodeCoverage/CodeCoverage/CoveredClass.swift
+++ CodeCoverage/CodeCoverage/CoveredClass.swift
@@ -8,6 +8,30 @@
 import UIKit
 
 class CoveredClass {
+    
+    private(set) var area: Int
+    
+    var widht: Int {
+        didSet {
+            area = widht * widht
+            let color: UIColor = redOrGreen(for: widht)
+            drawSquare(widht: widht, color: color)
+        }
+    }
+    
+    init() {
+        area = 0
+        widht = 0
+    }
+    
+    private func redOrGreen(for width: Int) -> UIColor {
+        width % 2 == 0 ? .red : .green
+    }
+    
+    private func drawSquare(widht: Int, color: UIColor) {
+        
+    }
+    
     static func max(_ x: Int, _ y: Int) -> Int {
         if x < y {
             return y
@@ -15,5 +39,12 @@ class CoveredClass {
         return x
     }
     
-    static func 
+    static func commaSeparated(from: Int, to: Int) -> String {
+        var result = ""
+        for i in from..<to {
+            result += "\(i),"
+        }
+        result += "\(to)"
+        return result
+    }
 }
--- CodeCoverage/CodeCoverageTests/CoveredClassTest.swift
+++ CodeCoverage/CodeCoverageTests/CoveredClassTest.swift
@@ -20,4 +20,20 @@ class CoveredClassTest: XCTestCase {
         let result = CoveredClass.max(5, 4)
         XCTAssertEqual(result, 5)
     }
+    
+    func test_commaSeparatedFrom2to4_shouldReturn234SeperatedByCommas() {
+        let result = CoveredClass.commaSeparated(from: 2, to: 4)
+        XCTAssertEqual(result, "2,3,4")
+    }
+    
+    func test_commaSeperatedFrom2to2_shouldReturn2() {
+        let result = CoveredClass.commaSeparated(from: 2, to: 2)
+        XCTAssertEqual(result, "2")
+    }
+    
+    func test_area_withWidth7_shouldBe49() {
+        let sut = CoveredClass()
+        sut.widht = 7
+        XCTAssertEqual(sut.area, 49)
+    }
 }
